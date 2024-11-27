65c3c95e112c7e0777e6b600e7a1b9e4dce71a1b
j <jhooghoudt@gmail.com>
2024-11-27 10:38:57 +0000

    Add validation and suggest user action on failing to discover model type / model base through the download url

--- src/cmds/download.py
+++ src/cmds/download.py
@@ -18,6 +18,17 @@ def run_download():
         model_base = sanitize_and_validate_arg_input(args.model_base, 'model_base_names')
     else:
         model_type, model_base = get_model_info(args.url)
+
+    ## make sure we have the required info
+    if not model_type or not model_base:
+        raise ValueError(f"\n\n----------------------------------------------------\n"
+                         f"Could not determine model type or base model..\n"
+                         f"model_type: {model_type}, model_base: {model_base} \n"
+                         f"----------------------------------------------------\n"
+                         f"Please try again by passing in the type and the base\n"
+                         f"more explicitly e.g.:\n\n"
+                         f"> cozy download <url> controlnet sd15\n")
+    
     download_dir = os.path.join(storage_root_dir, model_type, model_base)
 
     # Check if file exists, and download if necessary
